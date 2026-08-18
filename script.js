b0c472228ffddf47c0bf0ecb98c832f9c77cdc5c
JMortonPhotography <jordyn.coffman@gmail.com>
2026-08-18 10:05:52 -0500

    Refactor booking form submission with fetch API
    Updated form submission process to use fetch API and handle response.

--- script.js
+++ script.js
@@ -23,13 +23,28 @@ navigationLinks.forEach(function (link) {
 
 });
 const bookingForm = document.querySelector(".booking-form");
-const formSuccess = document.querySelector("#form-success");
+const formSuccess = document.querySelector(".form-success");
 
 if (bookingForm) {
-    bookingForm.addEventListener("submit", function () {
-        setTimeout(function () {
-            bookingForm.reset();
-            formSuccess.style.display = "blcok";
-        }, 1000);
-    });
+  bookingForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(bookingForm);
+
+    fetch("https://script.google.com/macros/s/AKfycbzs1ocfPLZQhtnwjGumIAzw_643lGjVWKJmEQ7E5mBSC8UhEJEGoZQQsfIN-asKJg9o6w/exec", {
+      method: "POST",
+      body: new URLSearchParams(formData)
+    })
+      .then(() => {
+        bookingForm.reset();
+
+        if (formSuccess) {
+          formSuccess.style.display = "block";
+        }
+      })
+      .catch((error) => {
+        console.error("Form submission error:", error);
+        alert("Something went wrong. Please try again.");
+      });
+  });
 }
